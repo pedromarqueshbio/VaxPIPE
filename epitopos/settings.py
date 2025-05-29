c695eae6f464816f81ba64de3554d4a21e3bb5e4
João Vinícius <vinivieiran@outlook.com>
2025-05-28 21:00:45 -0300

    Novas configurações pra funcionar no luar.

--- epitopos/settings.py
+++ epitopos/settings.py
@@ -28,6 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]  # Allow all hosts for development; change in production
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://vaxpipe.luar.dcc.ufmg.br",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+
 
 # Application definition
 
